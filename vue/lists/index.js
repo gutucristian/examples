fa3399fefdb37d2ea835169b3442fc1c2000c96e
Cristian Gutu <gutucristian27@gmail.com>
2020-05-14 22:04:37 -0400

    lists

---
+++ vue/lists/index.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['cherries', 'apples', 'oranges'],
+    people: [
+      {firstName: 'Bob', lastName: 'Smith', age: 28},
+      {firstName: 'Jen', lastName: 'Doe', age: 25}
+    ]
+  }
+});
