08eea75dc0e7c8e91b85e0cdd7638325569acb17
Cristian Gutu <gutucristian27@gmail.com>
2020-05-16 23:02:27 -0400

    initial implementation of game controls

--- vue/monster_game/index.js
+++ vue/monster_game/index.js
@@ -1,28 +1,52 @@
 new Vue({
   el: '#app',
   data: {
-    isGameOver: false,
+    isGameOver: true,
     playerWon: false,
     player: {
       name: "Foo",
-      health: 100
+      health: 100,
+      hits: []
     },
     monster: {
       name: "Monster",
-      health: 100
-    }
+      health: 100,
+      hits: []
+    },
   },
   methods: {
-    attack: function(event, weight = 20) {            
-      const damage = Math.floor((Math.random() * weight) + 1); 
-      this.monster.health -= damage;
+    attack: function(event, weight = 20) {
+      const monsterDamage = this.getRandomInt({max: weight});
+      const playerDamage = this.getRandomInt({max: weight});            
+
+      if (this.monster.health - playerDamage <= 0) {
+        this.monster.health = 0;
+        this.playerWon = true;
+        this.isGameOver = true;
+      } else {
+        this.monster.health -= playerDamage;
+      }
+
+      if (this.player.health - monsterDamage <= 0) {
+        this.player.health = 0;
+        this.playerWon = false;
+        this.isGameOver = true;
+      } else {
+        this.player.health -= monsterDamage;
+      }
+
+      this.monster.hits.push(playerDamage);
+      this.player.hits.push(monsterDamage);
     },
     heal: function(event, weight = 20) {
-      const health = (Math.random() * weight) + 1
-      this.player.health += health;      
+      const health = this.getRandomInt({max: weight});
+      this.player.health = this.player.health + health > 100 ? 100 : this.player.health + health;
     },
     getWinner: function() {
       return player.health < 0 ? 'Monster' : 'You';
+    },
+    getRandomInt: function({min = 1, max = 100} = {min: 5, max: 100}) {      
+      return Math.floor((Math.random() * max) + min);      
     }
   }
 });
