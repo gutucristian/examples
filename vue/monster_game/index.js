d9f9cf1329d8b9f3d7585c227f810844cc421e45
Cristian Gutu <gutucristian27@gmail.com>
2020-05-17 11:22:11 -0400

    finish gameplay logs

--- vue/monster_game/index.js
+++ vue/monster_game/index.js
@@ -1,36 +1,45 @@
 new Vue({
   el: '#app',
   data: {
-    isGameOver: true,
-    playerWon: false,
-    player: {
-      name: "Foo",
+    gameOver: true,
+    playerWon: undefined,
+    player: {    
       health: 100,
       hits: []
     },
-    monster: {
-      name: "Monster",
+    monster: {      
       health: 100,
       hits: []
     },
   },
+  computed: {
+    cockpitMessage: function() {
+      if (this.playerWon) {
+        return 'You won! Play again.';
+      } else if (this.playerWon === false) {
+        return 'You lost! Play again.';
+      } else {
+        return 'Play';
+      }
+    }
+  },
   methods: {
-    attack: function(event, weight = 20) {
-      const monsterDamage = this.getRandomInt({max: weight});
-      const playerDamage = this.getRandomInt({max: weight});            
+    attack: function(playerAttackWeight, monsterAttackWeight) {
+      const monsterDamage = this.getRandomInt({max: monsterAttackWeight});
+      const playerDamage = this.getRandomInt({max: playerAttackWeight});            
 
       if (this.monster.health - playerDamage <= 0) {
         this.monster.health = 0;
-        this.playerWon = true;
-        this.isGameOver = true;
+        this.gameOver = true;
+        this.playerWon = true;    
       } else {
         this.monster.health -= playerDamage;
       }
 
       if (this.player.health - monsterDamage <= 0) {
         this.player.health = 0;
+        this.gameOver = true;
         this.playerWon = false;
-        this.isGameOver = true;
       } else {
         this.player.health -= monsterDamage;
       }
@@ -42,11 +51,16 @@ new Vue({
       const health = this.getRandomInt({max: weight});
       this.player.health = this.player.health + health > 100 ? 100 : this.player.health + health;
     },
-    getWinner: function() {
-      return player.health < 0 ? 'Monster' : 'You';
-    },
     getRandomInt: function({min = 1, max = 100} = {min: 5, max: 100}) {      
       return Math.floor((Math.random() * max) + min);      
-    }
+    },
+    initGame: function() {
+      this.gameOver = false;
+      this.playerWon = undefined;      
+      this.player.health = 100;
+      this.monster.health = 100;
+      this.player.hits = []
+      this.monster.hits = []
+    }    
   }
 });
