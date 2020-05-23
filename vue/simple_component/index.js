56ddf7f0c1d651c7fae5ca34a8d85ac87fd9c2e1
Cristian Gutu <gutucristian27@gmail.com>
2020-05-23 14:47:06 -0400

    registering components

--- vue/simple_component/index.js
+++ vue/simple_component/index.js
@@ -1,5 +1,7 @@
-data = { status: 'Critical' };
+// data = { status: 'Critical' };
 
+/*
+// Example below regisers component GLOBALLY
 Vue.component('my-cmp', {
   data: function () {
     return {
@@ -17,7 +19,30 @@ Vue.component('my-cmp', {
   },
   template: '<p>Server status: {{ status }} (<button @click="changeStatus">Change</button>)</p>'
 });
+*/
+
+// Example below regisers component LOCALLY
+var cmp = {
+  data: function () {
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+    changeStatus: function () {
+      this.status = 'Normal';
+    }
+  },
+  template: '<p>Server status: {{ status }} (<button @click="changeStatus">Change</button>)</p>'
+};
 
 new Vue({
-  el: '#app'  
+  el: '#app',
+  components: {
+    'my-cmp': cmp // register component declared above LOCALLY to this instance ONLY 
+  }
+});
+
+new Vue({
+  el: '#app2'  
 });
