d1fb2485a9320b5e466d286b895088fee515d8fd
Cristian Gutu <gutucristian27@gmail.com>
2020-05-24 14:00:22 -0400

    local vs global component registering

--- vue/components/src/main.js
+++ vue/components/src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
-import Home from './components/Home.vue'
+import App from './components/App.vue'
 
 Vue.config.productionTip = false
-Vue.component('app-server-status', Home) // init custom selector for Home component
+// Vue.component('app-servers', Home) // init custom selector for Home component
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
