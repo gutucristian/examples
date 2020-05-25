721f9dc4b6538767903016659e1cf64ed39c3f6c
Cristian Gutu <gutucristian27@gmail.com>
2020-05-25 09:54:53 -0400

    update components-basic-practice

---
+++ vue/components-basic-practice/src/components/Servers.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li 
+        class="list-group-item"
+        v-for="index in 5"
+        :key="Math.random() + index">
+        Server #{{ index }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
