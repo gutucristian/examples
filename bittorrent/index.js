f20669e40cae154a6232a04d4ed52e021efdc6fd
Cristian Gutu <gutucristian27@gmail.com>
2020-06-08 19:38:53 -0400

    added functionality to get torrent file info hash

--- bittorrent/index.js
+++ bittorrent/index.js
@@ -1,11 +1,39 @@
 const fs = require('fs');
 const bencode = require('bencode');
+const crypto = require('crypto');
 
-// const torrent = bencode.decode(fs.readFileSync('kafka.torrent')));
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+const torrent = fs.readFileSync('kafka.torrent');
 
-console.log(torrent);
+/**
+ * Get torrent info hash.
+ * @param  {Buffer} torrent
+ * @return String   info hash
+*/
+function getInfoHash(torrent) {
+	if (Buffer.isBuffer(torrent)) {
+		torrent = bencode.decode(torrent);
+	}
 
-Object.keys(torrent).forEach(key => {
-	console.log(`${key}: ${torrent[key]}`);
-});
+	// sanity check
+	ensure(torrent.info, 'info')
+	
+	let shasum = crypto.createHash('sha1')
+	shasum.update(bencode.encode(torrent.info))
+	return shasum.digest('hex')
+}
+
+function ensure(bool, fieldName) {
+	if (!bool) throw new Error(`Torrent is missing require field: ${fieldName}`)
+}
+
+console.log(getInfoHash(torrent))
+
+// let shasum = crypto.createHash('sha1');
+// shasum.update(torrent.info);
+// shasum.digest('hex')
+// 
+// console.log(shasum);
+// 
+// Object.keys(torrent).forEach(key => {
+// 	console.log(`${key}: ${torrent[key]}`);
+// });
